405dfa55b316be1205bf484a7bd7565aad73a6e0
Miguel Angel Giraldo Polanco <miguel@MacBook-Pro-de-Miguel.local>
2026-04-28 13:27:25 +0200

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,20 +23,10 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    { url: '/favicon_io/favicon-16x16.png'},
+    { url: '/favicon_io/favicon-32x32.png'}, 
+  ],
+    apple: '/favicon_io/apple-touch-icon.png',
   },
 }
 
