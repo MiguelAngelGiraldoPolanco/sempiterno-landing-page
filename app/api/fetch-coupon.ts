1fdeb3507eb24ef3cf844ce70230211e3fc8d4d4
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-16 11:03:36 +0200

    create: fecth for coupons

---
+++ app/api/fetch-coupon.ts
@@ -0,0 +1,40 @@
+
+export interface Coupon {
+    id: number;
+    name: string;
+    create_at: string;
+    finish_at: string;
+    discount: number;
+}
+
+
+const API_URL = "http://localhost:8000/api/v1/coupons";
+
+
+export async function getAllCoupons(): Promise<Coupon[]> {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error("Error al obtener los cupones");
+    return await response.json();
+}
+
+export async function createCoupon(nuevoCoupon: Coupon): Promise<Coupon> {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(nuevoCoupon),
+    });
+    if (!response.ok) throw new Error("Error al crear el cupon");
+    return await response.json();
+}
+
+export async function getAllCouponsByDate(init_date: string, finish_date: string): Promise<Coupon[]> {
+    const response = await fetch(`${API_URL}/reporte/fechas?inicio=${init_date}&fin=${finish_date}`);
+    if (!response.ok) throw new Error("Error al obtener los cupones por fechas");
+    return await response.json();
+}
+
+export async function getCouponByName(name: string): Promise<Coupon> {
+    const response = await fetch(`${API_URL}/name/${name}`);
+    if (!response.ok) throw new Error("Error al obtener el cupon por el nombre");
+    return await response.json();
+}
